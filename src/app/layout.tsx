d391d5f80d9428d152226a8c6463747f25381d88
DoublePainz <ratraksmey4@gmail.com>
2025-01-06 10:29:06 +0700

    002

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Poppins, Space_Grotesk } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 
