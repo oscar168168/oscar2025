7106e10924b60af96d7bf1ac00dddd5ca19091b7
DoublePainz <ratraksmey4@gmail.com>
2025-01-10 15:04:04 +0700

    002

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -4,7 +4,6 @@ import React from "react";
 const Hero = () => {
   return (
     <main className="container mx-auto p-8 flex flex-col justify-center items-start lg:min-h-screen min-h-[95vh] lg:gap-12 gap-8">
-      
       <h1 className="bg-clip-text text-transparent bg-gradient-to-b from-foreground to-foreground/60">
         Discover Your <span className="text-primary">Uniqueness</span>, Unlock
         Your <span className="text-primary">Creativity</span>.
@@ -15,10 +14,10 @@ const Hero = () => {
         through <span className="text-primary">UX/UI Design</span>.
       </p>
       <div className="flex gap-4">
-        <Link href="/portfolio" className="pBtn">
+        <Link href="#portfolio" className="pBtn">
           View My Work
         </Link>
-        <Link href="/contact" className="sBtn">
+        <Link href="https://t.me/doublepainz" className="sBtn">
           Let&apos;s Collaborate
         </Link>
       </div>
